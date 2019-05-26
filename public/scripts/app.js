909628f16d036b77c26e573b9d3c27a27defd60d
Kepa Tairua <kepa.tairua@gmail.com>
2019-05-26 21:51:32 +1200

    Set up app for Heroku

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,9 +4,12 @@ const messageTwoEl = document.getElementById("message-2")
 
 weatherForm.addEventListener("submit", (e) => {
     e.preventDefault()
+    
     messageOneEl.innerHTML = "<img id='loading' src='/images/loading.gif'>"
     messageTwoEl.textContent = ""
+
     const address = e.target[0].value.trim()
+
     fetch(`/weather?address=${address}`).then((response) => {
         response.json().then((data) => {
             messageOneEl.innerHTML = ""
